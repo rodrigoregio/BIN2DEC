31b3fbf190161d8e3fe65433747d999731ae6b0b
rodrigo <rodrigo@rodrigoLNX>
2020-02-28 10:47:58 -0300

    Melhorando o teste das condições Java e comentando a classe de conversão

--- src/main/java/top/rregio/bin2dec/dominio/Conversion.java
+++ src/main/java/top/rregio/bin2dec/dominio/Conversion.java
@@ -54,9 +54,11 @@ public class Conversion {
             exibeErroConformeExecucao(modoExec,"O campo não deve estar vazio!");
 
         } else if (texto.length() > 8) {
-            //Senão testa a Segunda condição (se o tamanho do texto for maior que 8 caracteres)
+            //Senão testa a Segunda condição (se o tamanho do texto for maior 
+            //que 8 caracteres)
 
-            exibeErroConformeExecucao(modoExec,"O campo não deve conter mais que oito caracteres!");
+            exibeErroConformeExecucao(modoExec,"O campo não deve conter mais "+
+                    "que oito caracteres!");
         } else {//Senão (se as duas condições forem satisfeitas)
             for (int i = 0; i < texto.length(); i++) {
                 //em todos caracteres da string
@@ -66,7 +68,8 @@ public class Conversion {
                 } else {//Senão 
                     
                     //chama o metodo que exibe a mensagem de acordo com a execução
-                    exibeErroConformeExecucao(modoExec,"O campo não deve conter caracteres diferentes de \'0\' e \'1\'!");
+                    exibeErroConformeExecucao(modoExec,"O campo não deve conter "+
+                            "caracteres diferentes de \'0\' e \'1\'!");
                     
                     //Atribua 0 á variavel teste
                     teste = 0;
@@ -109,21 +112,44 @@ public class Conversion {
     }
 
     public void CommandLineConversion() {
+    //Inicia a conversão pela linha de comando
+        
+        //Inicializa a classe Scanner do pacote java.util
         Scanner sc = new Scanner(System.in);
 
+        //inicializa a variavel teste com 0
         teste = 0;
 
         do {
+        //faça isto
+            
             System.out.println("Digite um numero Binário:");
+            //Solicita que o usuario digite um numero binario
+            
             binario = sc.nextLine();
+            //lê o que foi digitado pelo usuario
+            
             teste = testaCondicoes(binario, 0);
+            //a variavel teste vai receber o retorno do método testa condições
+            
             if (teste == 0) {
+            //verifica se a variavel teste é igual a 0
+                
                 continue;
+                //se for ignora todos os comando abaixo deste continue
             } else {
+            //senão
                 bin = Integer.parseInt(binario, 2);
+                //realiza a conversão da String em um valor inteiro que está 
+                //na base dois (binário)
             }
-
+            //E se a variavel teste não for 0 continua com a execução dos 
+            //comandos abaixo:
+            
             StringBuilder sb = new StringBuilder();
+            //Instancia uma String Builder para mostrar o valor digitado e o 
+            //valor já convertido
+            //A String Builder começa com uma String vazia
             sb.append("Valores do binario:\n\n");
             sb.append("Decimal: ");
             sb.append(bin);
@@ -131,19 +157,39 @@ public class Conversion {
             sb.append("Binario: ");
             sb.append(binario);
             sb.append("\n");
+            //Os comandos acima apenas acrescentam a String passada como 
+            //parametro á String do StringBuilder
             System.out.println(sb);
+            //Imprime a String do StringBuilder
             teste = 1;
+            //a variavel teste recebe 1
+            
         } while (teste == 0);
+        //enquanto a variavel teste for igual a 0 (se a variavel teste for 1 
+        //sairá do loop)
     }
 
+    /**
+     * Método que executa no modo Janela
+     * 
+     * Variavel binario é o texto a ser convertido em decimal
+     * @param binario
+     * 
+     * Retorna um inteiro com o resultado da conversão
+     * @return 
+     */
     public int WindowedConversion(String binario) {
-        teste = testaCondicoes(binario, 1);
-        if (teste == 0) {
-
-        } else {
+    //inicio do metodo em modo janela
+    
+        teste = testaCondicoes(binario, 1); // binario é a string a ser validada
+        //e 1 é o modo de execução. A variavel teste recebe o resultado da
+        //validação da String.
+        
+        if (teste != 0) {
+        //Se a variavel teste for diferente de 0 (no caso 1) 
             bin = Integer.parseInt(binario, 2);
+            //se for diferebte de 0 realiza a conversão para binario
         }
-        teste = 1;
         return bin;
     }
 }
